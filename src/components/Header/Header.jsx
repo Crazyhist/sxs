4bd578acc671f8953bc73a4395fb98936166be36
Boss <crazyhistorian2000@yandex.ru>
2024-01-30 02:34:50 +0300

    2 cum

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,35 @@
+import logoImg from '../../assets/images/logo.jpg'
+
 import styles from './header.module.css'
 
 function Header() {
 	return (
-		<>
-			<header>
-				<a className={styles.header} href=''>
-					{' '}
-					Hello{' '}
-				</a>
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-			</header>
-		</>
+		<header className={styles.header}>
+			<div className='max-w-[1440px] border-4 m-auto px-[120px]'>
+				<div className={styles.header_row}>
+					<div className={styles.header_logo}>
+						<img src={logoImg} alt='SOSI' />
+					</div>
+					<nav className=''>
+						{[
+							['MAIN', '/dashboard'],
+							['ABOUT', '/team'],
+							['GAME FEATURES', '/projects'],
+							['SYSTEM REQUIREMENTS', '/reports'],
+							['QUOTES', '/quotes'],
+						].map(([title, url]) => (
+							<a
+								href={url}
+								className='rounded-lg px-3 py-2 text-slate-700 font-medium hover:text-orange-500'
+							>
+								{title}
+							</a>
+						))}
+					</nav>
+					<div className={styles.header_navLink}></div>
+				</div>
+			</div>
+		</header>
 	)
 }
 
